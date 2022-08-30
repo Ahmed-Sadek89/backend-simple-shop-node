c17fdd09aebd696bbd87d0621e0cd38f3853e6db
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-08-30 17:03:43 +0200

    backend-simple-shop-node

---
+++ Middlewares/VerifyToken.guard.js
@@ -0,0 +1,60 @@
+const JWT = require('jsonwebtoken');
+
+const verifyToken = ( req, res, next ) => {
+    const tokenHeader = req.headers.token;
+    if (tokenHeader) {
+        const token = tokenHeader.split(" ")[1];
+        JWT.verify(token, process.env.JWT_SECRET_KEY, async (error, user ) => {
+            if (error) {
+                //console.log(error);
+                res.status(404).json({
+                    status: 404,
+                    tokenError: error.message //invalid token
+                })
+            } else {
+                console.log(user);
+                // user => contain all info that you sign to JWT in auth.controller(login)
+                req.user = user
+                next()
+            }
+        })
+    } else {
+        res.status(404).json({
+            status: 404,
+            tokenError: 'you are not authenticated'
+        })
+    }
+}
+
+const verifyTokenAndAuthorization = ( req, res, next ) => {
+    verifyToken( req, res, () => {
+        if ( req.user.id || req.user.isAdmin ) {
+            console.log('paramID ',req.user.id)
+            next()
+        } else {
+            res.status(404).json({
+                status: 404,
+                tokenError: 'you are not authorized'
+            })
+        }
+    })
+}
+
+const verifyTokenAndAdmin = ( req, res, next ) => {
+    verifyToken( req, res, () => {
+        if ( req.user.isAdmin === true ) {
+            next()
+        } else {
+            res.status(404).json({
+                status: 404,
+                tokenError: 'you are not admin'
+            })
+        }
+    })
+}
+
+module.exports = {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+}
